31a1d48ce5f555aceb70431aa00876971beb9136
sunjesse <jessera@outlook.com>
2023-02-24 15:33:51 -0500

    added ineq constraints

--- src/index.py
+++ src/index.py
@@ -1,11 +1,12 @@
 import itertools
 import numpy as np
+from utils import *
 
 class Indexer():
 	def __init__(self, nx, ny):
 		self.nx = nx
 		self.ny = ny
-		self.n = self.nx + self.ny
+		self.n = self.nx*self.ny
 
 	def get_vec(self, a):
 		'''
@@ -32,7 +33,23 @@ class Indexer():
 		nus = np.zeros((1 << self.n, 1)) 
 		return np.concatenate(XY, axis=-1).T, nus
 
+	def get_ineq(self):
+		B = []
+		seen = set()
+		for b in range(1, 1 << self.n):
+			b_vec = self.get_vec(b)
+			# since mu is linear, positions where a AND b == 1 cancel out.
+			# ==> can just do xor
+			subsets = [b^a for a in range(b) if is_subset(a, b)]
+			for x in subsets:
+				if x in seen: continue
+				B.append(self.get_vec(x)[:, np.newaxis])
+				seen.add(x)
+		return np.concatenate(B, axis=-1).T, np.zeros((len(B), 1))
+
+
 if __name__ == '__main__':
-	idxr = Indexer(5, 5)
-	W = idxr.get_Wb()
-	print(W)
+	idxr = Indexer(3,3)
+	B, z = idxr.get_ineq()
+	print(B)
+	print(B.shape, z.shape)
--- src/main.py
+++ src/main.py
@@ -2,17 +2,21 @@ import numpy as np
 import cvxpy as cp
 from index import Indexer
 
-dimX = 5
-dimY = 5
-dim = dimX + dimY
+dimX = 3
+dimY = 3
+dim = dimX*dimY
+
 l1 = lambda C, x : cp.sum(cp.abs(C @ x))
 
 indexer = Indexer(dimX, dimY)
 W, b = indexer.get_Wb()
+B, zeros = indexer.get_ineq()
 M = cp.Variable((dim, 1))
+
+# A := Mobius transform
 A = np.random.rand(dim, dim)
 
-constraints = [W @ M == b]
+constraints = [W @ M == b, B @ M >= zeros]
 
 print(constraints)
 prob = cp.Problem(cp.Minimize(l1(A, M)), constraints)
