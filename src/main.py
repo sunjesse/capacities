09b1337ed951865c8ce8310a083aff6783ff590a
sunjesse <jessera@outlook.com>
2023-02-28 16:43:37 -0500

    added l1 norm trick

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 import numpy as np
 import cvxpy as cp
+import argparse
 from index import Indexer
 
+# arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--lp', action='store_true')
+args = parser.parse_args()
+
+ 
 dimX = 3
 dimY = 3
 dim = dimX*dimY
-
-l1 = lambda C, x : cp.sum(cp.abs(C @ x))
 
 indexer = Indexer(dimX, dimY)
 W, b = indexer.get_Wb()
@@ -15,10 +20,22 @@ M = cp.Variable((dim, 1))
 
 # A := Mobius transform
 A = np.random.rand(dim, dim)
-
 constraints = [W @ M == b, B @ M >= zeros]
 
-print(constraints)
-prob = cp.Problem(cp.Minimize(l1(A, M)), constraints)
-sol = prob.solve()
-print(prob.value, M.value)
+if not args.lp:
+	print("Minimizing L_1 norm...")
+	l1 = lambda C, x : cp.sum(cp.abs(C @ x))
+
+	prob = cp.Problem(cp.Minimize(l1(A, M)), constraints)
+	sol = prob.solve()
+	print(f"Solution: {M.value} with value {sol}.")
+
+else:
+	print("Minimizing LP using L_1 norm trick...")
+	c = np.ones((dim, 1))
+	t = cp.Variable((dim, 1))
+	
+	prob = cp.Problem(cp.Minimize(c.T @ t),
+					  constraints + [A@M <= t, A@M >= -t])
+	sol = prob.solve()
+	print(f"Solution: {M.value} with value {sol}.")
